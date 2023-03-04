ee32ea59606be1ced5da5954875258992c6b5292
youssefkarfouh <youssefkarfouh7@gmail.com>
2023-03-04 18:18:03 +0100

    changes to feature/swagger branch

--- src/main/java/ma/pfe/PfeApplication.java
+++ src/main/java/ma/pfe/PfeApplication.java
@@ -1,13 +1,18 @@
 package ma.pfe;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class PfeApplication {
 
     public static void main(String[] args) {
 
+       // private final static Logger LOGGER =
         SpringApplication.run(PfeApplication.class);
 
     }
--- src/main/java/ma/pfe/controllers/StudentController.java
+++ src/main/java/ma/pfe/controllers/StudentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class StudentController {
 
+    String test;
     private final static Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 
     private StudentService service;
